278bf5cbc363e749c724ef31f44f87a2053aabc9
John Sullivan <jpsullivan2019@gmail.com>
2020-10-01 20:08:26 -0400

    implements most basic possible site generation

---
+++ pydown.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+# pydown.py
+# exploratory SSG for fisher.nd.edu
+# see design.md for rationale
+
+# imports
+import markdown
+
+# test header and footer
+header = """<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+    <title>Test Website</title>
+</head>
+
+<body>"""
+footer = """</body>
+</html>"""
+
+# markdown file
+MD_FILE = 'test_content.md'
+HTML_OUT = 'index.html'
+
+# main to whack everything together with some markdown read in
+def main():
+    
+    with open(MD_FILE, 'r') as input_f:
+        text = input_f.read()
+
+    html = markdown.markdown(text)
+
+    output = (header + html + footer)
+    with open(HTML_OUT, 'w') as out_f:
+        out_f.write(output)
+
+if __name__ == "__main__":
+    main()
---
+++ test_content.md
@@ -0,0 +1,7 @@
+# This is an H1
+
+## This is an H2
+This is some body text under that H2.
+
+## This is another H2
+More body content!!!
